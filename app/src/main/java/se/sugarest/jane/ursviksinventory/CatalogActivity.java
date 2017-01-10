07472d81dd886f0b233722ec9ed8030f6404980b
JinyanLiu <jane.jinyan.liu@gmail.com>
2017-01-10 13:18:03 +0100

    Edit activity_editor xml file. And set up empty view in CatalogActivity. And set OnClickListener on "ADD" button.

--- app/src/main/java/se/sugarest/jane/ursviksinventory/CatalogActivity.java
+++ app/src/main/java/se/sugarest/jane/ursviksinventory/CatalogActivity.java
@@ -1,7 +1,11 @@
 package se.sugarest.jane.ursviksinventory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Find the "ADD" button.
+        Button addButton = (Button) findViewById(R.id.button_add_inventory);
+
+        // Set a click listener on that view
+        addButton.setOnClickListener(new Button.OnClickListener() {
+            // The code in this method will be executed when the "ADD" button is clicked on.
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the ListView which will be populated with the products data
+        ListView productListView = (ListView) findViewById(R.id.list_view_inventory);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has
+        // 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
     }
 }
---
+++ app/src/main/java/se/sugarest/jane/ursviksinventory/EditorActivity.java
@@ -0,0 +1,18 @@
+package se.sugarest.jane.ursviksinventory;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jane on 1/10/17.
+ */
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
