51ac678017c18afbb1eacda6253c517c664b2e48
JinyanLiu <jane.jinyan.liu@gmail.com>
2017-01-11 21:52:13 +0100

    Set up InventoryCursorAdapter java class.

---
+++ app/src/main/java/se/sugarest/jane/ursviksinventory/InventoryCursorAdapter.java
@@ -0,0 +1,94 @@
+package se.sugarest.jane.ursviksinventory;
+
+/**
+ * Created by jane on 1/11/17.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import se.sugarest.jane.ursviksinventory.data.InventoryContract;
+
+/**
+ * {@link InventoryCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of inventory data as its data source. This adapter knows
+ * how to create list items for each row of inventory data in the {@Link Cursor}.
+ */
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the inventory data (in the current row pointed to by curosr) to the given
+     * list item layout. For example, the name for the current inventory can be set on the name
+     * TextView in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        // Find individual views that we want to modify in the list item layout.
+        ImageView pictureImageView = (ImageView) view.findViewById(R.id.list_item_picture);
+        TextView nameTextView = (TextView) view.findViewById(R.id.list_item_name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.list_item_price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.list_item_quantity);
+
+        // Find the columns of inventory attributes that we're interested in
+        int pictureColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PICTURE);
+        int nameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+
+        // Read the attributes from the Cursor for the current pet\
+        byte[] imgByte = cursor.getBlob(pictureColumnIndex);
+        Bitmap productPicture = BitmapFactory.decodeByteArray(imgByte, 0, imgByte.length);
+        String productName = cursor.getString(nameColumnIndex);
+        int productPrice = cursor.getInt(priceColumnIndex);
+        int productQuantity = cursor.getInt(quantityColumnIndex);
+
+        // If the product picture is null, then set the imageview invisible
+        if (productPicture == null) {
+            pictureImageView.setVisibility(View.INVISIBLE);
+        }
+
+        // Update the ImageView and TextViews with the attributes for the current product
+        pictureImageView.setImageBitmap(productPicture);
+        nameTextView.setText(productName);
+        priceTextView.setText(productPrice);
+        quantityTextView.setText(productQuantity);
+    }
+}
